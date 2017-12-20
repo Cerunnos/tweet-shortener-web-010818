1a078e72cc992c0b83c4ccc499f1e45f5130b8d0
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-20 19:25:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,15 +1,18 @@
-dictionary={
-  "hello"=>"hi",
-  "to"=>"2"
-  "two"=>"2"
-  "too"=>"2"
-  "for"=>"4"
-  "four"=>"4"
-  "be"=>"b"
-  "you"=>"u"
-  "at"=>"@"
-  "and"=>"&"
-}
+def dictionary
+  dictionary={
+    "hello"=>"hi",
+    "to"=>"2"
+    "two"=>"2"
+    "too"=>"2"
+    "for"=>"4"
+    "four"=>"4"
+    "be"=>"b"
+    "you"=>"u"
+    "at"=>"@"
+    "and"=>"&"
+  }
+end
 def word_substituter(tweet)
   var1=tweet.to_a
+  var1.
 end
